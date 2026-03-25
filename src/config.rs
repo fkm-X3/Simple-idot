bc4f017209fdda257dcbdd0d302e19b7a53c28e9
justagame-dev <fkmbloodshead@gmail.com>
2026-03-26 10:35:15 +1100

    ignore logic

--- src/config.rs
+++ src/config.rs
@@ -4,6 +4,8 @@ pub struct IdotConfig {
     pub ignore_docs: bool, // whether to ignore ~ comments in the token stream
     #[allow(dead_code)] // same here
     pub allow_external_res: bool, // whether to allow external resources (bring(py) or bring(js))
+    #[allow(dead_code)] // and here
+    pub has_logic: bool, // whether to include logic that doesn't exist yet in the token stream
 }
 
 impl IdotConfig {
@@ -12,6 +14,7 @@ impl IdotConfig {
             version: "0.1.0",
             ignore_docs: true,      // set to false if you want to include ~ comments in the token stream
             allow_external_res: true,
+            has_logic: false, // ignore logic that doesn't exist yet
         }
     }
 }
